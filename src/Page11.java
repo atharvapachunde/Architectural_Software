65a2f8b30974e9426fab05791f381d74801ea63d
Akash Pandit AR <arpandit3010@gmail.com>
2025-03-10 20:53:40 +0530

    Akash Pandit 10/03/2025 Completed Chapter 3

--- src/Page11.java
+++ src/Page11.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JScrollBar;
+
 public class Page11 extends javax.swing.JFrame {
 //Heading Panel Variables
 
@@ -8,14 +11,14 @@ public class Page11 extends javax.swing.JFrame {
     String village = "";
     String taluka = "";
     double lowest = 0.0;
-   
-    public Page11()
-    {
+
+    public Page11() {
         initComponents();
     }
+
     public Page11(String surveyno, String gatno, String ctsno, String plotno, String village, String taluka, double lowest) {
         initComponents();
-         //Panel Code Initialization
+        //Panel Code Initialization
         this.plotno = plotno;
         this.gatno = gatno;
         this.surveyno = surveyno;
@@ -23,9 +26,12 @@ public class Page11 extends javax.swing.JFrame {
         this.village = village;
         this.taluka = taluka;
         this.lowest = lowest;
+        jTextArea1.setEditable(false);
+        jTextArea2.setEditable(false);
+        jTextArea3.setEditable(false);
+
     }
 
-  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -49,8 +55,30 @@ public class Page11 extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton4 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                formFocusLost(evt);
+            }
+        });
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowActivated(java.awt.event.WindowEvent evt) {
+                formWindowActivated(evt);
+            }
+        });
 
         jPanel1.setLayout(new java.awt.GridBagLayout());
 
@@ -218,6 +246,106 @@ public class Page11 extends javax.swing.JFrame {
             }
         });
 
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 937, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 235, Short.MAX_VALUE)
+        );
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setText("NET PLOT AREA AND COMPUTATION OF FSI");
+
+        jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        jTextArea1.setBackground(new java.awt.Color(242, 242, 242));
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("i) In case of a plotted layout / sub-division / group housing scheme / any development, net area shall be the balance  \nplot area after  deducting the area covered by amenity space under Regulation No.3.5 and Development / Regional Plan \nproposals including new roads and road widening, if any, from the total area of plot. \nii) For the purpose of computation of FSI / built-up area, the net area of the plot shall only be considered.  \niii) In case of plotted layout, the basic FSI of such net area shall be distributed on all plots on pro-rata basis or on certain\nplots as land owner desires, subject to maximum receiving potential prescribed in these regulations. \nHowever, such entitlement of FSI on certain plots shall be clearly mentioned on the layout plan.  \niv) In case of plots from already approved layouts, the plot area shall be treated as net plot area. \nv) The above regulations in respect of net plot area and computation of FSI shall apply to proposals in all land use zones.");
+        jScrollPane2.setViewportView(jTextArea1);
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel3.setText("TRANSFER OF LAND UNDER D.P. SITES (OTHER THAN D.P. ROAD) IN LIEU OF FSI ");
+
+        jScrollPane3.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        jTextArea2.setBackground(new java.awt.Color(242, 242, 242));
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jTextArea2.setText("i) If an owner hands over a reserved site to the Authority free of cost, they can use its FSI on the remaining land, equivalent to TDR.\nii) A transfer deed must be executed, and FSI details included in the development proposal.\niii) In plotted layouts, FSI can be distributed as per owner’s choice, within the maximum permissible limit based on road width (Regulation 6.3).\niv) The receiving plot must have a minimum 9m road frontage.\nv) Unused FSI can be claimed as TDR.\nvi) If FSI is utilized on-site, DRC procedure is not required.");
+        jScrollPane3.setViewportView(jTextArea2);
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel4.setText("RELOCATION OF D.P. / R.P. SITES / ROADS");
+
+        jScrollPane4.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        jTextArea3.setBackground(new java.awt.Color(242, 242, 242));
+        jTextArea3.setColumns(20);
+        jTextArea3.setRows(5);
+        jTextArea3.setText("A) Shifting of reservations is allowed without altering the reserved area but not permitted if:\n\ti) It is split into multiple parts.\n\tii) It moves beyond 500m from its original location.\n\tiii) It moves outside the owner’s holding.\n\tiv) The new location differs significantly in access, levels, or size.\n\tv) It has already been relocated under these regulations.\n\tvi) It involves geo-sensitive land (e.g., biodiversity zones, nallahs).\n\tvii) It is outside the sanctioned layout area.\n\nB) Approval Process:\n\ti) Authority handles the relocation and reports it to the Government & Town Planning Director.\n\tii) The Development Plan is deemed modified upon approval.\n\nC) Relocation to Another Owner’s Land:\n\ti) Allowed within 300m if the other owner consents via a registered deed and hands over land in \n\texchange for TDR.\n\tii) Restrictions of sub-regulations (d), (e), and (f) apply.\n\tiii) No layout submission is required from the consenting owner.\n\nD) Road Alignment Shifts:\n\ti) Allowed if inlet & outlet points remain unchanged and traffic flow is unaffected.\n\tii) Must be approved by the Divisional Joint Director of Town Planning for smaller municipalities and \n\tregional areas.\n\tiii) The Development/Regional Plan is deemed modified accordingly.");
+        jScrollPane4.setViewportView(jTextArea3);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 677, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(91, 91, 91)
+                        .addComponent(jLabel3))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane3))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(203, 203, 203)
+                        .addComponent(jLabel4))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane4)))
+                .addGap(285, 285, 285)
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(210, 210, 210)
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(24, 24, 24))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(382, Short.MAX_VALUE))))
+        );
+
+        jScrollPane1.setViewportView(jPanel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -225,20 +353,28 @@ public class Page11 extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(186, 186, 186)
+                                .addComponent(jButton4)
+                                .addGap(127, 127, 127)
+                                .addComponent(jButton3)))
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(186, 186, 186)
-                        .addComponent(jButton4)
-                        .addGap(133, 133, 133)
-                        .addComponent(jButton3)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 198, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 237, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
                     .addComponent(jButton4))
@@ -249,21 +385,78 @@ public class Page11 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        Page10 p10=new Page10(surveyno, gatno, ctsno, plotno, village, taluka, lowest);
+        Page10 p10 = new Page10(surveyno, gatno, ctsno, plotno, village, taluka, lowest);
         p10.setVisible(true);
         p10.pack();
         this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        Page12 p12=new Page12(surveyno, gatno, ctsno, plotno, village, taluka, lowest);
+        Page12 p12 = new Page12(surveyno, gatno, ctsno, plotno, village, taluka, lowest);
         p12.setVisible(true);
         p12.pack();
         this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
+    private void formFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_formFocusLost
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formFocusLost
+
+    private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
+        headingPanelActivation();
+
+        JScrollBar verticalScrollBar = jScrollPane2.getVerticalScrollBar();
+        verticalScrollBar.setValue(0);
+
+        JScrollBar verticalScrollBar1 = jScrollPane3.getVerticalScrollBar();
+        verticalScrollBar1.setValue(0);
+
+        JScrollBar verticalScrollBar2 = jScrollPane4.getVerticalScrollBar();
+        verticalScrollBar2.setValue(0);
+
+
+    }//GEN-LAST:event_formWindowActivated
+    public void headingPanelActivation() {
+        //setting visibility
+        ctsl.setVisible(false);
+        cts1.setVisible(false);
+        gatnol.setVisible(false);
+        gatno1.setVisible(false);
+        surveyno_l.setVisible(false);
+        survey.setVisible(false);
+        plotno1.setVisible(false);
+        plotnol.setVisible(false);
+
+        if (!surveyno.isEmpty()) {
+            surveyno_l.setVisible(true);
+            survey.setVisible(true);
+            survey.setText(surveyno);
+        }
+        if (!ctsno.isEmpty()) {
+            ctsl.setVisible(true);
+            cts1.setVisible(true);
+            cts1.setText(ctsno);
+        }
+        if (!gatno.isEmpty()) {
+            gatno1.setVisible(true);
+            gatnol.setVisible(true);
+            gatno1.setText(gatno);
+        }
+        if (!plotno.isEmpty()) {
+            plotno1.setVisible(true);
+            plotnol.setVisible(true);
+            plotno1.setText(plotno);
+        }
+
+        //Setting village, lowest area, taluka
+        larea.setText(String.valueOf(lowest));
+        taluka1.setText(taluka);
+        village1.setText(village);
+
+    }
+
     public static void main(String args[]) {
-       
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Page11().setVisible(true);
@@ -281,7 +474,19 @@ public class Page11 extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextArea3;
     private javax.swing.JLabel larea;
     private javax.swing.JLabel plotno1;
     private javax.swing.JLabel plotnol;
